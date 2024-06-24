0a7d717a1b06dce1323b0ffdc6bd763e203a1182
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2024-06-24 16:33:38 +0200

    feat: Add script to configure Ubuntu machine
    This commit adds a new script, `configure_machine.sh`, that can be used to configure an Ubuntu machine. The script checks if the OS is Ubuntu and if the `apt` package manager is installed. It then installs a list of required packages using `apt`. Additionally, it installs `zsh4humans` using either `curl` or `wget`.
    
    The purpose of this script is to automate the setup process for an Ubuntu machine, making it easier for users to install the necessary packages and tools.

---
+++ configure_machine.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Check if the OS is Ubuntu
+if [[ "$(uname -a)" == *"Ubuntu"* ]]; then
+	# Check if apt is installed
+	if ! command -v apt &> /dev/null; then
+		echo "apt package manager is not installed."
+		exit 1
+	fi
+
+	# Install required packages
+    sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
+    sudo apt update
+	packages=("alacritty" "exa" "bat" "fastfetch" "fzf" "ripgrep" "zsh" "curl" "wget" "git" "unzip" "zip" "htop")
+
+	for package in "${packages[@]}"; do
+		if ! dpkg -s "$package" &> /dev/null; then
+			echo "Installing $package..."
+			sudo apt install -y "$package"
+		else
+			echo "$package is already installed."
+		fi
+	done
+
+    # Install zsh4humans
+    if command -v curl >/dev/null 2>&1; then
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+    else
+        sh -c "$(wget -O- https://raw.githubusercontent.com/romkatv/zsh4humans/v5/install)"
+    fi
+
+else
+	echo "At the moment this script is only compatible with Ubuntu distribution."
+	exit 1
+fi
+
