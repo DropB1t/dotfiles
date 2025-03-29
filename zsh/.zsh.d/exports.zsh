91add14ef39fb5df01b4b4808c37ef0d1a491cb0
Yuriy Rymarchuk <yura.ita.com@gmail.com>
2025-03-29 16:02:28 +0100

    feat(zsh): add FZF Catppuccin color scheme options and refactor default options function

--- zsh/.zsh.d/exports.zsh
+++ zsh/.zsh.d/exports.zsh
@@ -48,36 +48,51 @@ if [ -d "$HOME/.spicetify" ]; then
   export PATH="$HOME/.spicetify":$PATH
 fi
 
-# FZF Catppuccin
-#export FZF_DEFAULT_OPTS=" \
-#--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-#--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-#--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
-
 _gen_fzf_default_opts() {
+  local color00='#282828'
+  local color01='#3c3836'
+  local color02='#504945'
+  local color03='#665c54'
+  local color04='#bdae93'
+  local color05='#d5c4a1'
+  local color06='#ebdbb2'
+  local color07='#fbf1c7'
+  local color08='#fb4934'
+  local color09='#fe8019'
+  local color0A='#fabd2f'
+  local color0B='#b8bb26'
+  local color0C='#8ec07c'
+  local color0D='#83a598'
+  local color0E='#d3869b'
+  local color0F='#d65d0e'
+  export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+  " --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
+  " --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
+  " --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
+}
 
-local color00='#282828'
-local color01='#3c3836'
-local color02='#504945'
-local color03='#665c54'
-local color04='#bdae93'
-local color05='#d5c4a1'
-local color06='#ebdbb2'
-local color07='#fbf1c7'
-local color08='#fb4934'
-local color09='#fe8019'
-local color0A='#fabd2f'
-local color0B='#b8bb26'
-local color0C='#8ec07c'
-local color0D='#83a598'
-local color0E='#d3869b'
-local color0F='#d65d0e'
-
-export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
-" --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
-" --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
-" --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
-
+# FZF Catppuccin
+_gen_fzf_catppuccin() {
+  local color00='#1e1e2e'
+  local color01='#313244'
+  local color02='#414868'
+  local color03='#585b70'
+  local color04='#cdd6f4'
+  local color05='#f5e0dc'
+  local color06='#f8bd96'
+  local color07='#f2d5cf'
+  local color08='#f38ba8'
+  local color09='#f2a272'
+  local color0A='#f9c74f'
+  local color0B='#a6e3a1'
+  local color0C='#94e2d5'
+  local color0D='#89b4fa'
+  local color0E='#cba6f7'
+  local color0F='#b9bbcf'
+  export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+  " --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D"\
+  " --color=fg:$color04,header:$color0D,info:$color0A,pointer:$color0C"\
+  " --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color0D"
 }
 
 _gen_fzf_default_opts
