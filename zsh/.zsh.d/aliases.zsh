65b84e4cbb1c04c06333cf4e3ed9d127a927b411
Yuriy Rymarchuk <45283261+DropB1t@users.noreply.github.com>
2026-08-09 13:57:16 +0200

    feat(zsh): add y function wrapper for yazi

--- zsh/.zsh.d/aliases.zsh
+++ zsh/.zsh.d/aliases.zsh
@@ -85,3 +85,11 @@ download_music_from_youtube() {
 update-ghostty() {
     curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh | bash
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	command yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+	command rm -f -- "$tmp"
+}
